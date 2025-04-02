5a1418ff7c9517e42830d6da813e47588b9a50ce
Bharathchandra13 <bharathchandrab1210.sse@saveetha.com>
2025-04-02 11:40:12 +0530

    Update carpoolSchema.js

--- module/carpoolSchema.js
+++ module/carpoolSchema.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-const RideSchema = new mongoose.Schema({
+const CarpoolSchema = new mongoose.Schema({
     pickupLocation: { type: String, required: true },
     dropoffLocation: { type: String, required: true },
     selectedDate: { type: Date, required: true },
-    availableSeats: { type: Number, required: true }, // Renamed from numberOfTravelers
-    driverName: { type: String, required: true },
-    carModel: { type: String, required: true },
-    price: { type: Number, required: true },
-    time: { type: String, required: true },
-    rating: { type: Number, required: true },
-    imageUrl: { type: String, required: true }
+    numberOfTravelers: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('Ride', RideSchema); // Renamed to match API
-
+module.exports = mongoose.model('Carpool', CarpoolSchema);
