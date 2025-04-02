cb3c991d7058196f642ab7f9685432cddef41bf6
Bharathchandra13 <bharathchandrab1210.sse@saveetha.com>
2025-04-02 10:12:18 +0530

    Add files via upload

---
+++ module/Booking.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    carModel: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    timeSlot: { type: String, required: true },
+    garageId: { type: mongoose.Schema.Types.ObjectId, ref: "Garage", required: true },
+    status: { type: String, default: "Completed" }
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
