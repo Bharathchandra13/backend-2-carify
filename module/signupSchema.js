c27b45278afa34712d72c5cc1880bff9db865dfa
Bharathchandra13 <bharathchandrab1210.sse@saveetha.com>
2025-04-06 12:51:37 +0530

    Update signupSchema.js

--- module/signupSchema.js
+++ module/signupSchema.js
@@ -5,6 +5,8 @@ const signupSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
+    role: { type: String, required: true, enum: ["user", "garage"] }, // role added with validation
+    image: { type: String, required: false } // optional, in case you use it later
   },
   { timestamps: true }
 );
