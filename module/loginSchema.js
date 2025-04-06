b05d0df1c7e67f1aac1e8384bd399cd444e82bf8
Bharathchandra13 <bharathchandrab1210.sse@saveetha.com>
2025-04-06 12:41:32 +0530

    Update loginSchema.js

--- module/loginSchema.js
+++ module/loginSchema.js
@@ -1,11 +1,9 @@
+const mongoose = require("mongoose");
+
 const LoginSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
+        required: [true, "Please enter your name"]
     },
     email: {
         type: String,
@@ -16,8 +14,17 @@ const LoginSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please enter password"]
     },
+    role: {
+        type: String,
+        required: [true, "Please specify a role (e.g., user or garage)"]
+    },
     image: {
         type: String,
         required: false
     }
+}, {
+    timestamps: true,
 });
+
+const Login = mongoose.model("Login", LoginSchema);
+module.exports = Login;
